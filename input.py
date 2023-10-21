e6aca68d6fc13172ddc97ad698cdc70f6ff178fd
ndnghia2410 <91201271+ndnghia24@users.noreply.github.com>
2023-10-21 21:47:03 +0700

    update input and maze format

--- input.py
+++ input.py
@@ -1,8 +1,24 @@
 import os
 
+# lưu danh sách điểm thưởng, điểm đón
+mapping_dict = {}
+
+# lưu đường dẫn file maze
 maze_path = str(os.path.dirname(os.path.abspath(__file__))) + "\input\level_1\input1.txt"
 
 with open(maze_path, "r") as file:
+    n = int(file.readline())
+
+    for i in range(n):
+        buff = file.readline()
+        coordinates = buff.split(" ")
+        
+        x = int(coordinates[0])
+        y = int(coordinates[1])
+        val = int(coordinates[2])
+
+        mapping_dict[(x, y)] = val
+
     data = [line.rstrip() for line in file.readlines()]
 
 for i in data:
