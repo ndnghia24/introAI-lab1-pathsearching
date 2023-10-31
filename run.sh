ffa5bd8b9a0833df595049ba7e45108880786921
ndnghia2410 <91201271+ndnghia24@users.noreply.github.com>
2023-10-31 18:02:14 +0700

    update python3

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-python -m pip install --upgrade pip
+python3 -m pip install --upgrade pip
 pip install pygame argparse imageio numpy
 
-python source/main.py
+python3 source/main.py
 rmdir /s source/noBonus_algorithms/__pycache__
