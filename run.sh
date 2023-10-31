ac1e5b6957a434f7582320a8af69d40711fd0e63
ndnghia2410 <91201271+ndnghia24@users.noreply.github.com>
2023-10-31 23:13:37 +0700

    update

--- run.sh
+++ run.sh
@@ -3,4 +3,9 @@
 # python3 -m pip install --upgrade pip
 # pip install pygame argparse imageio numpy
 
+sudo apt-get install python3-pygame 
+sudo apt-get install python3-argparse 
+sudo apt-get install python3-imageio
+sudo apt-get install python3-numpy
+
 python3 ./source/main.py
